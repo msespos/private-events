c7fcfffce6bd88935edf85598d8c8d399ac4a5a5
Mike Esposito <mikeesposito6852@gmail.com>
2021-03-29 09:22:50 -0700

    Generate Users controller (had accidentally generated User controller and reverted)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resource :user, only: [:show, :new, :create]
-
 end
