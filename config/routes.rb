cfb968cbf6c3b967c914698bf4994212fc5894c9
Mike Esposito <mikeesposito6852@gmail.com>
2021-04-02 08:54:36 -0700

    Change resource :users to resources :users to allow for multiple user pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resource :users, only: [:show, :new, :create]
+  resources :users, only: [:show, :new, :create]
   resource :sessions, only: [:new, :create]
 
 end
