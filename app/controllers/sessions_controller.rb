177e46a6f848b9b93ad5d7332138584b9137de0d
Mike Esposito <mikeesposito6852@gmail.com>
2021-04-05 14:22:36 -0700

    Add :current_user_id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,5 +5,6 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_name(params[:name])
     session[:current_user_name] = user.name
+    session[:current_user_id] = user.id
   end
 end
