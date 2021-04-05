5cff64e03f95e4aee02f6dc323b360f641b7f3a5
Mike Esposito <mikeesposito6852@gmail.com>
2021-04-05 14:23:57 -0700

    Add #show and #new and #create and #event_params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,12 +3,24 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  def show
+  def event_params
+    params.require(:event).permit(:description, :creator_id)
   end
 end
