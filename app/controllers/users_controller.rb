8bc42667c81ba1eaf994737682cc127e01a58e48
Mike Esposito <mikeesposito6852@gmail.com>
2021-04-05 14:21:54 -0700

    Change users_path to root_path after save and require :user in params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path
+      redirect_to root_path
     else
       render :new
     end
   end
 
   def user_params
-    params.permit(:name, :email)
+    params.require(:user).permit(:name, :email)
   end
 end
