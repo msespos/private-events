4867e2f3b49f28cbf0a5929c9c5cfa0c8770072d
Mike Esposito <mikeesposito6852@gmail.com>
2021-04-07 18:35:00 -0700

    Remove unnecessary current_user_id session data and hidden field in Events New view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def current_user
-    @current_user ||= User.find_by(id: session[:current_user_id], name: session[:current_user_name])
+    @current_user ||= User.find_by(name: session[:current_user_name])
   end
 end
