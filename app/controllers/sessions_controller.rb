c73a535b8747e997aca1ae2cc81295f5d94cada7
Mike Esposito <mikeesposito6852@gmail.com>
2021-04-07 18:50:24 -0700

    Remove current_user_id from session data

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,5 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_name(params[:name])
     session[:current_user_name] = user.name
-    session[:current_user_id] = user.id
   end
 end
