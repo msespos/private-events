4362d27c8bb47ecde1b48f3831834333c8194f20
Mike Esposito <mikeesposito6852@gmail.com>
2021-04-09 09:21:32 -0700

    Add associations for has many through relationship between Event and User models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
+  has_many :tickets, foreign_key: :event_id
+  has_many :attendees, through: :tickets, source: :user
   belongs_to :creator, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :tickets, foreign_key: :user_id
+  has_many :attended_events, through: :tickets
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
 end
