01712f1fab5c12e1e2ec6d61f6f736a2a42b8e46
Mike Esposito <mikeesposito6852@gmail.com>
2021-04-10 17:59:05 -0700

    Add #past and #upcoming class methods to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,11 @@ class Event < ApplicationRecord
   has_many :tickets, foreign_key: :event_id
   has_many :attendees, through: :tickets, source: :user
   belongs_to :creator, class_name: "User"
+  def self.past
+    where('date < ?', DateTime.now)
+  end
+
+  def self.upcoming
+    where('date > ?', DateTime.now)
+  end
 end
