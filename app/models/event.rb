6c6525eae7b86d037a8a0fbcd3f7c4aaae6e821b
Mike Esposito <mikeesposito6852@gmail.com>
2021-04-10 18:13:24 -0700

    Refactor class methods into scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,11 +2,6 @@ class Event < ApplicationRecord
   has_many :tickets, foreign_key: :event_id
   has_many :attendees, through: :tickets, source: :user
   belongs_to :creator, class_name: "User"
-  def self.past
-    where('date < ?', DateTime.now)
-  end
-
-  def self.upcoming
-    where('date > ?', DateTime.now)
-  end
+  scope :past, -> { where('date < ?', DateTime.now) }
+  scope :upcoming, -> { where('date > ?', DateTime.now) }
 end
