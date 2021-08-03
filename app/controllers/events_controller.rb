22a9663c9e5f2368717fbc82f4c7c9d16526ca22
Mike Esposito <mikeesposito6852@gmail.com>
2021-08-03 14:35:34 -0700

    Add #new, #index, and #create to Events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,32 @@
 class EventsController < ApplicationController
 
   def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
   end
 
   def create
-    @event = current_user.created_events.build(creator_id: params[:id])
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      flash[:success] = "New post created!"
+      redirect_to events_path
+    else
+      flash.now[:error] = "Error in creating post"
+      render :new
+    end
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:id, :name, :description, :date, :time,
+                                   :location)
+    end
 end
