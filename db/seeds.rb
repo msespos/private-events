aeb323d87ce94ea558893fa136030da1fafdd799
Mike Esposito <mikeesposito6852@gmail.com>
2021-08-03 14:41:03 -0700

    Create User and Event seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "mike",
+             email: "mike@mike.mike",
+             password: "123456",
+             password_confirmation: "123456")
+
+99.times do |n|
+  name = Faker::Movies::HitchhikersGuideToTheGalaxy.character
+  email = "email-#{n+1}@email.email"
+  password = "123456"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
+25.times do
+  name = Faker::Music::RockBand.name
+  description = Faker::TvShows::TheITCrowd.quote
+  date = Faker::Date.forward(days: 23)
+  time = Faker::Time.forward(days: 5,  period: :evening, format: :long)
+  location = Faker::Movies::StarWars.planet
+  creator_id = rand(1..100)
+  Event.create!(name: name,
+                description: description,
+                date: date,
+                time: time,
+                location: location,
+                creator_id: creator_id)
+end
