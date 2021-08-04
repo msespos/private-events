055860def8d13e70ad68df7299d741984c7d7473
Mike Esposito <mikeesposito6852@gmail.com>
2021-08-03 18:17:44 -0700

    Amend associations for attendees and attended events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+  has_many :tickets, foreign_key: :attended_event_id
+  has_many :attendees, through: :tickets
   belongs_to :creator, class_name: "User"
-  belongs_to :attendee, class_name: "User"
 end
