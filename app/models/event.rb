48ad5921de479e0db3c827a92d9db58e7a6ba49c
Mike Esposito <mikeesposito6852@gmail.com>
2021-08-13 16:10:58 -0700

    Add Event.past and Event.future class methods and display past and future events on the Event Index Page

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   has_many :tickets, foreign_key: :attended_event_id
   has_many :attendees, through: :tickets
   belongs_to :creator, class_name: "User"
+
+  def self.past
+    order("date desc").where("date <= ?", Date.today)
+  end
+
+  def self.future
+    order("date asc").where("date > ?", Date.today)
+  end
 end
