c25db90802d7803c7319e2c7667d526e6aaacda8
Mike Esposito <mikeesposito6852@gmail.com>
2021-08-13 09:17:08 -0700

    Add source to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tickets, foreign_key: :attendee_id
-  has_many :attended_events, through: :tickets
+  has_many :attended_events, through: :tickets, source: :event
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
 end
