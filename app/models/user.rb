167faa311862a11e2141814addab2bf6fc7aacb6
Mike Esposito <mikeesposito6852@gmail.com>
2021-08-25 07:18:26 -0700

    Add ticket_for method for use in _events_display partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   has_many :tickets, foreign_key: :attendee_id
   has_many :attended_events, through: :tickets, source: :event
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
+
+  def ticket_for(event)
+    tickets.where('attended_event_id = ?', event.id).first
+  end
 end
