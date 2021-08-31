30c659b1e41b06d4c7f87c7669d9521439daa060
Mike Esposito <mikeesposito6852@gmail.com>
2021-08-31 14:54:22 -0700

    Add name to sign up and log in and remove email from log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {
+                               |u| u.permit(:name, :email, :password,
+                                            :password_confirmation) }
+    devise_parameter_sanitizer.permit(:sign_in) {
+                               |u| u.permit(:name, :password) }
+    devise_parameter_sanitizer.permit(:account_update) {
+                               |u| u.permit(:name, :email, :password,
+                                            :password_confirmation,
+                                            :current_password) }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:name]
   has_many :tickets, foreign_key: :attendee_id
   has_many :attended_events, through: :tickets, source: :event
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
