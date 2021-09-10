40e3a70cf09d7a532df6c99af470b10403a9b875
Mike Esposito <mikeesposito6852@gmail.com>
2021-09-10 10:21:15 -0700

    Add uniqueness to name seeds and adjust date and time seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,8 @@ User.create!(name: "mike",
              password: "123456",
              password_confirmation: "123456")
 
-99.times do |n|
-  name = Faker::Movies::HitchhikersGuideToTheGalaxy.character
+49.times do |n|
+  name = Faker::Movies::HitchhikersGuideToTheGalaxy.unique.character
   email = "email-#{n+1}@email.email"
   password = "123456"
   User.create!(name: name,
@@ -24,10 +24,10 @@ end
 25.times do
   name = Faker::Music::RockBand.name
   description = Faker::Movies::BackToTheFuture.quote
-  date = Faker::Date.forward(days: 23)
-  time = Faker::Time.forward(days: 5,  period: :evening, format: :long)
+  date = Faker::Date.between(from: 1.month.ago, to: Date.today + 1.month)
+  time = Faker::Time.between(from: 1.month.ago, to: DateTime.now + 1.month, format: :long)
   location = Faker::Movies::StarWars.planet
-  creator_id = rand(1..100)
+  creator_id = rand(1..50)
   Event.create!(name: name,
                 description: description,
                 date: date,
