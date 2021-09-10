523e2d3f1020c8c107ac627942442eb262ef3bfc
Mike Esposito <mikeesposito6852@gmail.com>
2021-09-10 14:03:03 -0700

    Add validations for name, date, and time

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,9 @@ class Event < ApplicationRecord
   has_many :tickets, foreign_key: :attended_event_id
   has_many :attendees, through: :tickets, source: :user, dependent: :destroy
   belongs_to :creator, class_name: "User"
+  validates :name, presence: true, uniqueness: true
+  validates :date, presence: true
+  validates :time, presence: true
 
   scope :past, -> { order("date desc").where("date < ?", Date.today) }
   scope :future, -> { order("date asc").where("date > ?", Date.today) }
